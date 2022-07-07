fb3f8d2ba9d631cab2610393267684f08dcaf393
BautistaMendezC <bauti97my@gmail.com>
2022-07-07 18:55:37 -0300

    back terminado

--- client/src/componentes/Home.jsx
+++ client/src/componentes/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Card from "./Cards";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
@@ -9,41 +9,61 @@ import Paginado from "./Paginado";
 export default function Home () {
 
 
-const Dispatch = useDispatch ();
-const AllVideogames = useSelector((state=>state.videogames));
-const [currentPage, setCurrentPage] =useState(1);
-const [videogamesPerPage, setVideogamesPerPage] = useState(9);
-const indexOfLastVideogames = currentPage * videogamesPerPage; 
+const Dispatch = useDispatch ()
+const AllVideogames = useSelector((state=>state.videogames))
+const [currentPage, setCurrentPage] =useState(1)
+const [videogamesPerPage, setVideogamesPerPage] = useState(10)
+const indexOfLastVideogames = currentPage * videogamesPerPage;
 const indexOfFirstVideogames = indexOfLastVideogames - videogamesPerPage;
-const currentVideogames = AllVideogames.slice(indexOfFirstVideogames, indexOfLastVideogames);
+const currentVideogames = AllVideogames?.slice(indexOfFirstVideogames, indexOfLastVideogames)
+
 
 const paginado = (pageNumber) => {
     setCurrentPage(pageNumber)
-};
+ };
 useEffect(() => {
     Dispatch(getVideogames());
-    },[Dispatch])
+    },[Dispatch]);
 
-
-    
+function handleClick (e){
+    e.preventDefault();
+    Dispatch(getVideogames())
+}
     return(
         <div>
-            { currentVideogames && currentVideogames.map(e=>{
-            return(
-            <Card
-            name= {e.name}
-            released= {e.released}
-            rating= {e.rating}
-            platforms= {e.platforms}
-            img= {e.img}
-            genres= {e.genres}
-            />)})}
+            <Link to="/character"> Crear personaje</Link>
+            <h1> Bienvenido a tu pagina de videojuegos</h1>
+            <button onClick={e => {handleClick(e)}}>
+                 volver a cargar personajes
+            </button>
+            <select>
+                <option value="ascendente">ascendente</option>
+                <option value="descendente">descendente</option>
+            </select>
+            <select>
+                <option value="creadoDB">creados en DB</option>
+                <option value="creadosAPI">Creados en la API</option>
+            </select>
+
+            <div>
             <Paginado
                     videogamesPerPage ={videogamesPerPage}
-                    AllVideogames ={AllVideogames.length}
+                    AllVideogames ={AllVideogames?.length}
                     paginado ={paginado}
                 />
+
+            </div>
         
-        </div>
+            { 
+           currentVideogames?.map((e)=>{
+            return(
+                <Fragment>
+                <Link to={"/home/" + e.id}>
+            <Card name= {e.name} released= {e.released} rating= {e.rating} platforms= {e.platforms} img= {e.img} genres= {e.genres}/>
+            </Link>
+            </Fragment>
+            );
+            })}
+            </div>
     )
 };
